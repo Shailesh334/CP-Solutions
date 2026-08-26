6b7bb9b0b316b07de3b35487691e672015f2a0df
Shailesh Shivaji Gurle <shaileshgurle334@gmail.com>
2026-08-26 20:07:42 +0530

    Add Regular Cleaning (Unrated)

---
+++ codechef/Unrated/REGCLN - Regular Cleaning.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    
+    int n;
+    cin >> n;
+    
+    int last = n%10;
+    
+    cout << 10 - last << endl;
+}
