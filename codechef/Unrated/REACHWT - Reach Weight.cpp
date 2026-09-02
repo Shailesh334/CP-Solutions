29e9e17052f0434965b5b7bbcec40027ca4f5f28
Shailesh Shivaji Gurle <shaileshgurle334@gmail.com>
2026-09-02 20:05:34 +0530

    Add Reach Weight (Unrated)

---
+++ codechef/Unrated/REACHWT - Reach Weight.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    
+    int n;
+    cin >> n;
+    
+    int a = n/2;
+    int b = n%2;
+    
+    cout << a*30 + b*20 << endl;
+    
+}
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    solve();
+	}
+
+}
